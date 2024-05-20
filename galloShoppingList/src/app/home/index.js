5f47d75ae9a72d3b7c2b9387543e651019c6db5e
AnnaBeatrizSilva2904 <annabeatryz2504@gmail.com>
2024-05-20 12:24:29 -0300

    Finalização da interface

--- galloShoppingList/src/app/home/index.js
+++ galloShoppingList/src/app/home/index.js
@@ -1,23 +1,63 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
 import { StatusBar  } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
 import { styles } from './style';
 import ItemList from '../../components/ItemList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Home() {
+  const [textInput, setTextInput] = useState('');
+  const [items, setItems] = useState([]);
+
+  const saveItemToDevice = async () => {
+
+  }
+
+  const getItemsFromDevice = async() => {
+
+  }
+
+  const addItem = () => {
+
+  }
+
+  const markItemBought = itemId => {
+
+  }
+
+  const unmarkItemBought = itemId => {
+
+  }
+
+  const removeItem = itemId => {
+
+  }
+
+  const removeAll = () => {
+    Alert.alert('Limpar lista?', 'Confirme a exclusão de todos os produtos de sua lista?', 
+    [{
+      text: 'sim', onPress: () => {setItems([])}
+    },{
+      text: 'cancelar', style: 'cancel', 
+    }]);
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground source={require('../../assets/background.png')} resizeMode='repeat' style={{flex: 1, justifyContent: 'flex-start'}}>
         
         <View style={styles.header}>
           <Text style={styles.title}>Lista de produtos</Text>
-          <Ionicons name="trash" size={32} color='#fff'/>
+          <Ionicons name="trash" size={32} color='#fff' onPress={removeAll} />
         </View>
 
         {/* Lista de produtos */}
-        <ItemList item ={"Café"}></ItemList>
-        <ItemList item ={"Leite"}></ItemList>
+        <FlatList
+          contentContainerStyle={{ padding: 20, paddingBottom:100, color:"#fff" }}
+          data={items}
+          renderItem={({item}) => <ListItem item={item} markItem={markItemBought} unmarkItem={unmarkItemBought} removeItem={removeItem}/>}
+        />
 
         <View style={styles.footer}>
           <View style={styles.inputContainer}>
@@ -26,9 +66,11 @@ export default function Home() {
               fontSize={18}
               placeholderTextColor="#fff"
               placeholder='Digite o Nome do produto...'
+              value={textInput}
+              onChangeText={(text) => setTextInput(text)}
             />
           </View>
-          <TouchableOpacity style={styles.iconContainer}>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
             <Ionicons name='add' size={36} color='#fff'/>
           </TouchableOpacity>
         </View>
